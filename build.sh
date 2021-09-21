5619e3b081b996f92b7cace035ae2e4fab90060e
Alex Shemyakin <shemyakin@yoctocode.com>
2021-09-21 17:47:05 +0300

    update docker settings

---
+++ build.sh
@@ -0,0 +1,24 @@
+cd ./main
+docker build --tag arcadia-frontend:v1 .
+docker tag arcadia-frontend:v1 interestingstorage/arcadia-frontend:v1
+docker push interestingstorage/arcadia-frontend:v1
+
+cd ../backend
+docker build --tag arcadia-backend:v1 .
+docker tag arcadia-backend:v1 interestingstorage/arcadia-backend:v1
+docker push interestingstorage/arcadia-backend:v1
+
+cd ../app2
+docker build --tag arcadia-money-transfer:v1 .
+docker tag arcadia-money-transfer:v1 interestingstorage/arcadia-money-transfer:v1
+docker push interestingstorage/arcadia-money-transfer:v1
+
+cd ../app3
+docker build --tag arcadia-refer-a-friend:v1 .
+docker tag arcadia-refer-a-friend:v1 interestingstorage/arcadia-refer-a-friend:v1
+docker push interestingstorage/arcadia-refer-a-friend:v1
+
+cd ../Nginx
+docker build --tag arcadia-reverse-proxy:v1 .
+docker tag arcadia-reverse-proxy:v1 interestingstorage/arcadia-reverse-proxy:v1
+docker push interestingstorage/arcadia-reverse-proxy:v1
