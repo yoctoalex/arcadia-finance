682b6f8de12122d2c18441177c5b65e6634fca83
Alex Shemyakin <shemyakin@yoctocode.com>
2021-09-28 00:29:10 +0300

    rename folders

--- build.sh
+++ build.sh
@@ -8,17 +8,17 @@ docker build --tag arcadia-backend:v1 .
 docker tag arcadia-backend:v1 interestingstorage/arcadia-backend:v1
 docker push interestingstorage/arcadia-backend:v1
 
-cd ../app2
+cd ../money-transfer
 docker build --tag arcadia-money-transfer:v1 .
 docker tag arcadia-money-transfer:v1 interestingstorage/arcadia-money-transfer:v1
 docker push interestingstorage/arcadia-money-transfer:v1
 
-cd ../app3
+cd ../refer-a-friend
 docker build --tag arcadia-refer-a-friend:v1 .
 docker tag arcadia-refer-a-friend:v1 interestingstorage/arcadia-refer-a-friend:v1
 docker push interestingstorage/arcadia-refer-a-friend:v1
 
-cd ../Nginx
+cd ../nginx-reverse-proxy
 docker build --tag arcadia-reverse-proxy:v1 .
 docker tag arcadia-reverse-proxy:v1 interestingstorage/arcadia-reverse-proxy:v1
 docker push interestingstorage/arcadia-reverse-proxy:v1
