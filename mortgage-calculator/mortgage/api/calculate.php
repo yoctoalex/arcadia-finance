3ca00f27140c708a75377a2ef1c33545c23bd910
Alex Shemyakin <shemyakin@yoctocode.com>
2021-09-29 23:10:47 +0300

    update rates for zip

--- mortgage-calculator/mortgage/api/calculate.php
+++ mortgage-calculator/mortgage/api/calculate.php
@@ -30,14 +30,113 @@
     }
 
     function getInterestRate($zip) {
+        switch ($zip[0]) {
+            case "0":
+                return 0.02732;
+                break;
+            case "1":
+                return 0.025;
+                break;
+            case "2":
+                return 0.02455;
+                break;
+            case "3":
+                return 0.03115;
+                break;   
+            case "4":
+                return 0.03214;
+                break;   
+            case "5":
+                return 0.03115;
+                break;   
+            case "6":
+                return 0.03159;
+                break;                                                                            
+            case "7":
+                return 0.03299;
+                break;            
+            case "8":
+                return 0.02988;
+                break;    
+            case "9":
+                return 0.02678;
+                break;                                                                                  
+        }
+
         return 0.02875;
     }
 
     function getPropertyTaxRate($zip) {
+        switch ($zip[0]) {
+            case "0":
+                return 0.007971;
+                break;
+            case "1":
+                return 0.007855;
+                break;
+            case "2":
+                return 0.007691;
+                break;
+            case "3":
+                return 0.007712;
+                break;   
+            case "4":
+                return 0.007917;
+                break;   
+            case "5":
+                return 0.007983;
+                break;   
+            case "6":
+                return 0.007765;
+                break;                                                                            
+            case "7":
+                return 0.007839;
+                break;            
+            case "8":
+                return 0.007847;
+                break;    
+            case "9":
+                return 0.007882;
+                break;                                                                                  
+        }
+
         return 0.007971;
     }
 
     function getInsuranceRate($zip) {
+        switch ($zip[0]) {
+            case "0":
+                return 0.00313;
+                break;
+            case "1":
+                return 0.002989;
+                break;
+            case "2":
+                return 0.00345;
+                break;
+            case "3":
+                return 0.00288;
+                break;   
+            case "4":
+                return 0.002956;
+                break;   
+            case "5":
+                return 0.002972;
+                break;   
+            case "6":
+                return 0.002887;
+                break;                                                                            
+            case "7":
+                return 0.002779;
+                break;            
+            case "8":
+                return 0.002851;
+                break;    
+            case "9":
+                return 0.002999;
+                break;     
+        }        
+
         return 0.003;
     }
 
