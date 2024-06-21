2061c5c5d5c696907253480ef9261539f3498af4
Alex Shemyakin <alex@upmix.it>
2024-06-21 20:07:11 +0200

    update nginx and openbanking

--- openbanking/entrypoint.sh
+++ openbanking/entrypoint.sh
@@ -3,6 +3,6 @@
 while true
 do
 	openapi2postmanv2 -s ./api/openbanking.json -p -o collection.json -O folderStrategy=Tags,requestParametersResolution=Example,optimizeConversion=false,stackLimit=50
-    newman run collection.json --env-var "baseUrl=${BASE_URL}/tpp"
+    newman run collection.json --env-var "baseUrl=${BASE_URL}"
 	sleep 1
 done
