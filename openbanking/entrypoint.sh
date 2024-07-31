d2341052596901bbbbe8f162ac4783483a98f823
Alex Shemyakin <alex@upmix.it>
2024-08-01 01:45:15 +0200

    update traffic tool

--- openbanking/entrypoint.sh
+++ openbanking/entrypoint.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-	openapi2postmanv2 -s ./api/openbanking.json -p -o collection.json -O folderStrategy=Tags,requestParametersResolution=Example,optimizeConversion=false,stackLimit=50
+  openapi2postmanv2 -s ./api/openbanking.json -p -o collection.json -O folderStrategy=Tags,parametersResolution=Example,stackLimit=50,schemaFaker=true
     newman run collection.json --env-var "baseUrl=${BASE_URL}"
-	sleep 1
+  sleep 1
 done
